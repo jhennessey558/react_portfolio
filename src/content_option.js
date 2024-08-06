91f25f38be36971fd1575a0335e8e459276d4173
Jeff Hennessey <155253001+jhennessey558@users.noreply.github.com>
2024-08-06 19:10:34 -0400

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,121 @@
+const logotext = "Jeff";
+const meta = {
+    title: "Jeff Hennessey",
+    description: "I’m Jeff Hennessey Full stack developer,currently working with .Net Stack, SQL, Postgresql, Angular, React, and Vue",
+};
+
+const introdata = {
+    title: "I’m Jeff Hennessey",
+    animated: {
+        first: "I write codes",
+        second: "........",
+        third: "Lots and lots of codes",
+    },
+    description: "Senior Software Engineer with over 12 years of experience specializing in full-stack development, cloud computing, and quality assurance. Proven track record of architecting scalable solutions, optimizing performance, and leading cross-functional teams to deliver high-impact software products. Expertise in C#, .NET, and cloud platforms, with a strong focus on creating robust, user-centric applications. Demonstrated ability to drive innovation, mentor junior developers, and translate complex technical concepts for diverse stakeholders.",
+    your_img_url: "https://i.ibb.co/GF3tbDj/landing.png",
+};
+
+const dataabout = {
+    title: "About Myself",
+    aboutme: "I am a Senior Software Engineer with over 12 years of experience in full-stack development, cloud computing, and quality assurance. My career has been marked by a proven ability to architect scalable solutions and optimize performance while leading cross-functional teams to deliver high-impact software products. I specialize in technologies such as C#, .NET, JavaScript, and various cloud platforms, focusing on creating robust, user-centric applications. My background includes significant achievements in performance optimization, API design, and implementing agile methodologies. I have successfully driven innovation within my teams, mentoring junior developers and effectively translating complex technical concepts for diverse stakeholders. Throughout my career, I've spearheaded projects that have significantly improved user satisfaction and operational efficiency. For instance, at 505 Games, I led the quality assurance strategy for 'Ghostrunner 2', achieving a 95% bug-free release. At TPNi, I played a pivotal role in launching a comprehensive CMS platform that reduced event planning time by 50% and increased revenue by 30%. I am passionate about fostering collaboration and continuous learning within teams, which I believe is essential for driving success in today’s fast-paced tech environment.",
+};
+const worktimeline = [{
+        jobtitle: "505Games",
+        where: "Calabasas, California",
+        date: "2/2022 – 12/2023",
+    },
+    {
+        jobtitle: "TPNI",
+        where: "Dedham, Massachusetts",
+        date: "12/2011 - 12/2021",
+    },
+   
+];
+
+const skills = [{
+    name: "Languages: C#, .NET, JavaScript, PHP, SQL",
+    value: 100,
+},
+
+
+    {
+        name: "Web Technologies: React, Vue, Angular, jQuery, HTML5, CSS, Bootstrap, Tailwind",
+        value: 100,
+    },
+    {
+        name: "Cloud Platforms: Microsoft Azure, AWS",
+        value: 100,
+    },
+    {
+        name: "Databases: SQL Server, MySQL, PostgreSQL",
+        value: 100,
+    },
+    {
+        name: "Tools & Methodologies: Git, VSTS, Agile, SCRUM, DevOps",
+        value: 100,
+    },
+    {
+        name: "Specialties: REST API, Entity Framework, LINQ, Microservices Architecture",
+        value: 100,
+    },
+];
+
+const services = [{
+        title: "Cross-functional Leadership",
+        description: "At 505 Games, I led a team to enhance user satisfaction by 25% through advanced testing frameworks, demonstrating my ability to coordinate cross-functional efforts. At TPNi, I managed a team of 8 engineers to architect and launch a comprehensive CMS platform, integrating insights from various teams to reduce event planning time by 50% and increase revenue by 30%. My focus on collaboration has been key to driving innovation and delivering impactful results in my projects.",
+    },
+    {
+        title: "Cloud Architecture (Azure, AWS)",
+        description: "I have over 12 years of experience in cloud architecture, specializing in Microsoft Azure and Amazon Web Services (AWS). I have successfully designed and implemented scalable solutions, including a microservices architecture at TPNi that supported user growth from 10,000 to 100,000 while achieving 99.9% uptime for applications with over 1 million daily active users. At 505 Games, I optimized cross-platform performance, improving rendering speeds by 40%. My expertise in security, cost management, and resource optimization ensures the delivery of effective and sustainable cloud solutions.",
+    },
+    {
+        title: "Full-Stack Development",
+        description: "I have over 12 years of experience in full-stack development, proficient in technologies such as C#, .NET, JavaScript, React, Angular, and PHP. I led the design and launch of a comprehensive CMS platform at TPNi, reducing event planning time by 50% and increasing revenue by 30%. My focus on user-centric design, collaboration with cross-functional teams, and commitment to agile methodologies enable me to deliver high-quality applications that meet evolving user needs and enhance overall functionality.",
+    },
+    {
+        title: "Performance Optimization",
+        description: "At 505 Games, I architected a cross-platform performance optimization solution that improved rendering speeds by 40% across multiple platforms, including Windows, Linux, PlayStation 5, and Xbox Series X. This achievement was made possible through the strategic use of DirectX 12, Vulkan, and NVIDIA RTX technologies, ensuring that the gaming experience was both smooth and immersive.  During my tenure at TPNi, I led database optimization initiatives that enhanced data accuracy by 20% and improved query performance by 40%. By leveraging advanced SQL database integrations and indexing strategies, I ensured that our applications could handle increased loads without compromising speed or reliability.  My commitment to data-driven decision-making has also played a crucial role in my performance optimization efforts. I initiated data-driven product improvements using tools like Google Analytics and Mixpanel, which resulted in a 20% increase in positive user reviews and informed future development roadmaps. Through these experiences, I have developed a comprehensive skill set that enables me to identify performance bottlenecks and implement targeted solutions effectively.",
+    },
+    {
+        title: "Agile/SCRUM Methodologies",
+        description: "Extensive experience in implementing Agile and Scrum methodologies to enhance project efficiency and team collaboration. Responsibilities include facilitating daily stand-ups, sprint planning, and retrospectives to ensure alignment with project goals. Successfully led cross-functional teams at TPNi in delivering a CMS platform that reduced event planning time by 50%. A strong commitment to iterative development and continuous feedback has driven high-quality software delivery and improved stakeholder satisfaction across various projects.",
+    },
+    {
+        title: "Database Management",
+        description: "Proficient in database management with a strong focus on performance optimization and data integrity. At TPNi, led initiatives that improved data accuracy by 20% and enhanced query performance by 40% through advanced SQL database integrations and indexing strategies. Experienced in designing and maintaining robust database architectures that support scalable applications, ensuring efficient data handling and reliability for complex enterprise systems. My approach emphasizes best practices in security and resource management to deliver effective database solutions.",
+    },
+    {
+        title: "DevOps Practices",
+        description: "Skilled in DevOps practices that enhance collaboration between development and operations teams, streamlining workflows and improving deployment efficiency. At 505 Games, implemented CI/CD pipelines using Azure DevOps, enabling automated testing and faster release cycles. Experience in containerization with Docker and orchestration with Kubernetes, facilitating scalable and reliable application deployment. My focus on automation, monitoring, and continuous improvement has significantly reduced deployment times and increased system reliability across various projects.",
+    },
+];
+
+
+
+const contactConfig = {
+    YOUR_EMAIL: "jhennessey558@gmail.com",
+    YOUR_FONE: "(617)217-1906",
+    description: "Jeff Hennessey is a Senior Software Engineer with over 12 years of experience in full-stack development, cloud computing, and quality assurance. He has a proven track record of architecting scalable solutions and leading cross-functional teams to deliver impactful software products. With expertise in C#, .NET, and cloud platforms, Jeff is dedicated to creating robust, user-centric applications. He excels in mentoring junior developers and communicating complex technical concepts effectively. For inquiries, you can reach him at (617) 217-1906 or jhennessey558@gmail.com..... Or use the blatantly obvious form ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_ycb0qxo",
+    YOUR_TEMPLATE_ID: "template_i3g73nr",
+    YOUR_USER_ID: "lhaxPcLOgvDTxidal",
+};
+
+const socialprofils = {
+    github: "https://github.com/jhennessey558",
+    linkedin: "https://linkedin.com/in/jeff-hennessey-6b310215",
+    
+};
+export {
+    meta,
+    dataabout,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
