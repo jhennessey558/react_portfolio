e7c47dc83bd6f7cfd3be7e4740aba295ef4a687d
Jeff Hennessey <jhennessey558@gmail.com>
2024-08-07 02:08:19 -0400

    updates 1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './app/App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
     <App />
 );
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Container, Row, Col } from "react-bootstrap";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa"; // Importing icons
 import {
   dataabout,
   meta,
@@ -11,6 +12,17 @@ import {
 } from "../../content_option";
 
 export const About = () => {
+  const [expandedWork, setExpandedWork] = useState(null);
+  const [expandedSkills, setExpandedSkills] = useState(null);
+
+  const toggleWork = (index) => {
+    setExpandedWork(expandedWork === index ? null : index);
+  };
+
+  const toggleSkills = (index) => {
+    setExpandedSkills(expandedSkills === index ? null : index);
+  };
+
   return (
     <HelmetProvider>
       <Container className="About-header">
@@ -21,7 +33,7 @@ export const About = () => {
         </Helmet>
         <Row className="mb-5 mt-3 pt-md-3">
           <Col lg="8">
-            <h1 className="display-4 mb-4">About me</h1>
+            <h1 className="display-4 mb-4" class="aboutTransition">About me</h1>
             <hr className="t_border my-4 ml-0 text-left" />
           </Col>
         </Row>
@@ -35,33 +47,36 @@ export const About = () => {
             </div>
           </Col>
         </Row>
-        <Row className=" sec_sp">
+        <Row className="sec_sp">
           <Col lg="5">
-            <h3 className="color_sec py-4">Work Timline</h3>
+            <h3 className="color_sec py-4">Work Timeline</h3>
           </Col>
           <Col lg="7">
             <table className="table caption-top">
               <tbody>
-                {worktimeline.map((data, i) => {
-                  return (
-                    <tr key={i}>
-                      <th scope="row">{data.jobtitle}</th>
+                {worktimeline.map((data, i) => (
+                  <tr key={i} onClick={() => toggleWork(i)} className="clickable-row">
+                    <th scope="row">{data.jobtitle}</th>
+                    <td>{data.where}</td>
+                    <td>{data.date}</td>
+                    <td>
+                      <div className="progress-bar" style={{ width: `${data.value}%` }}>
+                        <div className="progress-value"></div>
+                      </div>
+                    </td>
+                    <td>
+                      {expandedWork === i ? <FaChevronUp /> : <FaChevronDown />}
                       
-                      <td>{data.where}</td>
-                      <td>{data.date}</td>
-                      <div
-                      className="progress-bar"
-                      style={{
-                        width: `${data.value}%`,
-                      }}
-                    >
-                      <div className="progress-value"></div>
-                    </div>
-                    </tr>
-                  );
-                })}
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
+            {expandedWork !== null && (
+              <div className="work-details">
+                <p>{worktimeline[expandedWork].description}</p>
+              </div>
+            )}
           </Col>
         </Row>
         <Row className="sec_sp">
@@ -69,38 +84,38 @@ export const About = () => {
             <h3 className="color_sec py-4">Skills</h3>
           </Col>
           <Col lg="7">
-            {skills.map((data, i) => {
-              return (
-                <div key={i}>
-                  <h3 className="progress-title">{data.name}</h3>
-                  <div className="progress">
-                    <div
-                      className="progress-bar"
-                      style={{
-                        width: `${data.value}%`,
-                      }}
-                    >
-                      <div className="progress-value"></div>
-                    </div>
+            {skills.map((data, i) => (
+              <div key={i} onClick={() => toggleSkills(i)} className="clickable-skill">
+                <h3 className="progress-title">{data.name}</h3>
+                <div className="progress">
+                  <div className="progress-bar" style={{ width: `${data.value}%` }}>
+                    <div className="progress-value"></div>
                   </div>
                 </div>
-              );
-            })}
+                <div className="skill-toggle-icon">
+                  {expandedSkills === i ? <FaChevronUp /> : <FaChevronDown />}
+                </div>
+                {expandedSkills === i && (
+                  <div className="skill-details">
+                    <p>{data.description}</p>
+                  </div>
+                )}
+              </div>
+            ))}
           </Col>
         </Row>
         <Row className="sec_sp">
-          <Col lang="5">
+          <Col lg="5">
             <h3 className="color_sec py-4">Core Competencies</h3>
           </Col>
           <Col lg="7">
-            {services.map((data, i) => {
-              return (
-                <div className="service_ py-4" key={i}>
-                  <h5 className="service__title">{data.title}</h5>
-                  <p className="service_desc">{data.description}</p>
-                </div>
-              );
-            })}
+            {services.map((data, i) => (
+              <div className="service_ py-4" key={i}>
+                <h5 className="service__title">{data.title}</h5>
+                <p className="service_desc">{data.description}</p>
+                
+              </div>
+            ))}
           </Col>
         </Row>
       </Container>
