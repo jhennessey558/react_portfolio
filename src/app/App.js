f8960923a1a2713d663bc7cb9ffc36d0d2360ecc
Jeff Hennessey <jhennessey558@gmail.com>
2024-08-12 01:47:14 -0400

    update current project

--- src/app/App.js
+++ src/app/App.js
@@ -32,6 +32,7 @@ export default function App() {
           outerScale={5}
         />
       </div>
+      
       <ScrollToTop>
         <Headermain />
         <AppRoutes />
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -19,6 +19,7 @@ export const Home = () => {
             className="h_bg-image order-1 order-lg-2 h-100 "
             style={{ backgroundImage: `url(${introdata.your_img_url})` }}
           ></div>
+          
           <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
             <div className="align-self-center ">
               <div className="intro mx-auto">
@@ -37,6 +38,7 @@ export const Home = () => {
                     }}
                   />
                 </h1>
+                
                 <p className="mb-1x">{introdata.description}</p>
                 <div className="intro_btn-action pb-5">
                    {/* <Link to="/portfolio" className="text_2"> *
